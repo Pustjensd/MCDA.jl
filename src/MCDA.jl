d693384370f27eebc2bc0429c501a7d8054f67b6
Dennis Pustjens <dennispustjens@hotmail.com>
2019-02-28 12:54:09 +0100

    added a lot

--- src/MCDA.jl
+++ src/MCDA.jl
@@ -1,18 +1,56 @@
 module MCDA
 
-export Criterion, Concept
+export Criterion, Concept, addcriterion!
 
 struct Criterion
     name::String
     localweight::Float64
     globalweight::Float64
+    valueconverter::Function
+
+    #Criterion(name, localweight, globalweight) = new(name, localweight, globalweight, identity)
 end
 
 struct Concept
+    name::String
     criteria::Vector{Criterion}
     values::Vector
 
-    Concept() = new(Criterion[], [])
+    Concept(name, criteria) = new(name, criteria, Vector{Any}(undef, length(criteria)))
+end
+
+function convertedvalues(con)
+    numcrits = length(con.criteria)
+    output = zeros(numcrits)
+    for (i, (crit, val)) in enumerate(zip(con.criteria, con.values))
+        output[i] = crit.valueconverter(val)
+    end
+    return output
+end
+
+function addcriterion!(con::Concept, cr::Criterion, value)
+    push!(con.criteria, cr)
+    push!(con.values, value)
+end
+
+function convertSymbolicScale(val)
+    conversiondict = Dict([
+        ("ref", 5.0),
+        ("---", 10/14),
+        ("--", 30/14),
+        ("-", 50/14),
+        ("0", 70/14),
+        ("+", 90/14),
+        ("++", 110/14),
+        ("+++", 130/14)
+    ])
+    return conversiondict[val]
+end
+
+struct EvaluationMethods
+    criteria::Vector{Criterion}
+    evaluationmethod::String
+    range::String
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,49 @@
 using MCDA
 using Test
 
-c1 = Criterion("Maneuverability", 8, 0.115)
-c2 = Criterion("Signature profile", 9, 0.129)
-c3 = Criterion("Redundancy", 7, 0.101)
-c4 = Criterion("Nr. of components", 2, 0.023)
-c5 = Criterion("Space consumption", 5, 0.058)
-c6 = Criterion("Weight", 7, 0.081)
-c7 = Criterion("Fuel capacity", 7, 0.081)
-c8 = Criterion("Reliability", 4, 0.042)
-c9 = Criterion("Maintainability", 6, 0.064)
-c10 = Criterion("Shock-proofness", 3, 0.032)
-c11 = Criterion("Purchase costs", 7, 0.107)
-c12 = Criterion("Fuel costs", 9, 0.138)
-c13 = Criterion("Maintenance costs", 2, 0.031)
+c1 = Criterion("Maneuverability", 8, 0.115, MCDA.convertSymbolicScale)
+c2 = Criterion("Signature profile", 9, 0.129, MCDA.convertSymbolicScale)
+c3 = Criterion("Redundancy", 7, 0.101, identity)
+c4 = Criterion("Nr. of components", 2, 0.023, identity)
+c5 = Criterion("Space consumption", 5, 0.058, identity)
+c6 = Criterion("Weight", 7, 0.081, identity)
+c7 = Criterion("Fuel capacity", 7, 0.081, identity)
+c8 = Criterion("Reliability", 4, 0.042, MCDA.convertSymbolicScale)
+c9 = Criterion("Maintainability", 6, 0.064, MCDA.convertSymbolicScale)
+c10 = Criterion("Shock-proofness", 3, 0.032, MCDA.convertSymbolicScale)
+c11 = Criterion("Purchase costs", 7, 0.107, identity)
+c12 = Criterion("Fuel costs", 9, 0.138, identity)
+c13 = Criterion("Maintenance costs", 2, 0.031, identity)
 
+criteria = [c1, c2]
+
+#con1 = Concept(Criterion)
 @test c1.name == "Maneuverability"
+@test c2.localweight == 9
+
+@show con = Concept("concept 1", criteria)
+con.values[1] = "---"
+con.values[2] = "ref"
+@show con = Concept("concept 2", criteria)
+con.values[1] = "0"
+con.values[2] = "0"
+@show con = Concept("concept 3", criteria)
+con.values[1] = "--"
+con.values[2] = "-"
+@show con = Concept("concept 4", criteria)
+con.values[1] = "+++"
+con.values[2] = "++"
+@show con = Concept("concept 5", criteria)
+con.values[1] = "+++"
+con.values[2] = "++"
+@show con = Concept("concept 6", criteria)
+con.values[1] = "+"
+con.values[2] = "+"
+@show con = Concept("concept 7", criteria)
+con.values[1] = "++"
+con.values[2] = "+"
+@show con = Concept("concept 8", criteria)
+con.values[1] = "+++"
+con.values[2] = "+++"
+cvals = MCDA.convertedvalues(con)
+@show cvals
